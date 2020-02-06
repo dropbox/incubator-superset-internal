41b28c41c2c9538e441a270f827b0efb3f23d26b
bogdan kyryliuk <bogdankyryliuk@dropbox.com>
2020-02-06 09:26:07 -0800

    CTA -> CTAS and dict -> {}

--- tests/superset_test_config.py
+++ tests/superset_test_config.py
@@ -29,7 +29,7 @@ if "SUPERSET__SQLALCHEMY_DATABASE_URI" in os.environ:
     SQLALCHEMY_DATABASE_URI = os.environ["SUPERSET__SQLALCHEMY_DATABASE_URI"]
 
 SQL_MAX_ROW = 666
-SQLLAB_CTA_NO_LIMIT = True  # SQL_MAX_ROW will not take affect for the CTA queries
+SQLLAB_CTAS_NO_LIMIT = True  # SQL_MAX_ROW will not take affect for the CTA queries
 FEATURE_FLAGS = {"foo": "bar"}
 
 
